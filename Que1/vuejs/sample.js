b03886f9b571351f356f4d28f39f60d7773070a9
cdacpune1 <50952547+cdacpune1@users.noreply.github.com>
2020-01-24 19:28:02 +0530

    Add files via upload

---
+++ Que1/vuejs/sample.js
@@ -0,0 +1,29 @@
+new Vue({
+el:'#r1',
+data:{
+    //citynm:['Kolhapur','Pune','Mumbai','Satara'],
+    country:[
+        {name:'India',city:['kolhapur','Pune','Karad']},
+        {name :'China',city:['Beijing','Shanghai','Tokiyo']},
+        {name : 'France',city:['Cannes','Chartres','Limoges']}
+        ]
+}
+});
+new Vue({
+    el:'#r2',
+    data:{
+            
+      
+                      
+            
+     
+
+    }
+});
+new Vue({
+    el:'#r',
+    data:{
+        name :''
+    }
+})
+
